416b6773c2ed7677d8ac8117555edee1db8640da
nom-gv <63209740+nom-gv@users.noreply.github.com>
2021-12-15 19:34:35 -0400

    Add files via upload

---
+++ CodigoC/E1Ejercicio4/Index.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="E1Ejercicio4.Index" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+	<title>Cuarto Algoritmo</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="Estilo/Style.css">
+</head>
+<body>
+	<div class="body-cont">
+		<h1>CUARTO EJERCICIO</h1>
+		<h2>Mostrar n cantidad de primos</h2>
+		<form id="form1" runat="server">
+            <asp:TextBox ID="num1" runat="server" Width="100%" placeholder="Numero de datos a mostrar"></asp:TextBox>
+            <br>
+			<hr>
+			<label>Resultado: </label>
+            <asp:TextBox ID="resultado" runat="server" Width="100%"></asp:TextBox>
+			&nbsp;<hr>
+            <asp:Button ID="Button1" runat="server" Text="Mostrar Primos" Width="100%" OnClick="MostrarPrimos"/>
+		</form>
+	</body>
+</html>
---
+++ CodigoC/E1Ejercicio4/Index.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace E1Ejercicio4
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void MostrarPrimos(object sender, EventArgs e)
+        {
+            resultado.Text = mostrar(Int32.Parse(num1.Text));
+        }
+        private Boolean[] cribaEratostenes()
+        {
+            Boolean [] primo = new bool[100];
+            for (int i = 0; i < 100; i++)
+                primo[i] = true;
+            for (int i = 2; i * i < 100; i++)
+                if (primo[i] == true)
+                    for (int j = i * 2; j < 100; j+=i)
+                        primo[j] = false;
+            return primo;
+        }
+        String mostrar(int n)
+        {
+            Boolean [] primo = cribaEratostenes();
+            String cad = "";
+            int r = 0;
+            for(int i=2; i<100; i++)
+            {
+                if (r == n)
+                    break;
+                else
+                {
+                    if (primo[i] == true)
+                    {
+                        cad = cad + i + " ";
+                        r++;
+                    }
+                }
+            }
+            return cad;
+        }
+    }
+}
