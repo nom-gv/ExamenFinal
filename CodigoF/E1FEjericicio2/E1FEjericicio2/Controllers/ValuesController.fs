a8392c8902d6f606c0a2ad824d691d325ba2af15
nom-gv <63209740+nom-gv@users.noreply.github.com>
2021-12-15 15:26:16 -0400

    Add files via upload

---
+++ CodigoF/E1FEjericicio2/E1FEjericicio2/Controllers/ValuesController.fs
@@ -0,0 +1,28 @@
+﻿namespace E1FEjericicio2.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+
+[<Route("api/[controller]")>]
+[<ApiController>]
+type ValuesController () =
+    inherit ControllerBase()
+
+    let calculado x y z = 
+        if (z = "Suma") then
+            (fun a b -> a+b) x y
+        elif (z = "Resta") then 
+            (fun a b -> a-b) x y
+        elif (z = "Multiplicacion") then 
+            (fun a b -> a*b) x y
+        else 
+            (fun a b -> a/b) x y
+    
+    let res = calculado 5 4 "Suma"
+
+    [<HttpGet>]
+    member this.Get() = 
+        base.Ok(res)
