8f9801c5a4e2b7c956a2782712664ba51266f0e4
nom-gv <63209740+nom-gv@users.noreply.github.com>
2021-12-15 15:10:16 -0400

    Add files via upload

---
+++ CodigoF/E1Ejercicio1/Primero/E1Ejercicio1/WebApplication4/Controllers/Nuevo.fs
@@ -0,0 +1,28 @@
+﻿namespace WebApplication4
+
+open System
+open Microsoft.AspNetCore.Mvc
+open System.Collections.Generic
+
+type n = { name: string}
+
+[<ApiController>]
+[<Route("[controller]")>]
+type Nuevo() =
+    inherit ControllerBase()
+
+    let rec fibonacci x = 
+        if (x < 3) then
+            0
+        elif (x < 5) then 
+            1
+        else 
+            fibonacci (x - 1) + fibonacci (x - 2)
+    
+    let cad (x: int) = string x
+
+    let n = cad(fibonacci 1) + " " + cad(fibonacci 2) + " " + cad(fibonacci 3) + " " + cad(fibonacci 4) + " " + cad(fibonacci 5)
+
+    [<HttpGet>]
+    member this.Get() = 
+        base.Ok(n)
---
+++ CodigoF/E1Ejercicio1/Primero/E1Ejercicio1/WebApplication4/Controllers/ValuesController.fs
@@ -0,0 +1,28 @@
+﻿namespace WebApplication4.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+
+[<Route("api/[controller]")>]
+[<ApiController>]
+type ValuesController () =
+    inherit ControllerBase()
+
+    let rec fibonacci x = 
+        if (x < 3) then
+            0
+        elif (x < 5) then 
+            1
+        else 
+            fibonacci (x - 1) + fibonacci (x - 2)
+    
+    let cad (x: int) = string x
+
+    let n = cad(fibonacci 1) + " " + cad(fibonacci 2) + " " + cad(fibonacci 3) + " " + cad(fibonacci 4) + " " + cad(fibonacci 5)
+
+    [<HttpGet>]
+    member this.Get() = 
+        base.Ok(n)
